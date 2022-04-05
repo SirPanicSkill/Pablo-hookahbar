b419b20f05bcaebb8fab61cf9cf76a88ea2139cb
SirPanicSkill <txwnik@live.ru>
2022-04-05 17:02:24 +0300

    step to complete desktop version

--- app.js
+++ app.js
@@ -56,10 +56,12 @@ function doMainLogic() {
         el: '#App',
         data: {
             mainData: cardsData,
+            menuActive: '',
             modalOrder: {
                 isOpen: false,
             },
             price: 0,
+            scrollPos: 0,
             isCoreLoading: true,
             isMobile: false,
             currency: '₽',
@@ -69,17 +71,60 @@ function doMainLogic() {
         },
         created: function() {
             let self = this;
+
+            // check for mobile
+            window.innerWidth <= 666 ? self.isMobile = true : self.isMobile = false;
         },
         mounted: function() {
+            // preloader
             setTimeout(() => {
                 this.isCoreLoading = false;
             }, 1000);
+
+            this.$nextTick(function () {
+                // set menu active
+                this.menuActive = this.mainData[0].Title;
+                // get all menu offsetTop
+                this.mainData.map(element => {
+                    let elemOffsetTop = App.querySelector(`section[data-title='${ element.Title }']`).offsetTop;
+
+                    return element.offsetTop = elemOffsetTop;
+                });
+            });
+            
+            
+            // scroll + menu
+            App.addEventListener('scroll', () => {
+                let halfScreen = App.offsetHeight / 2;
+                let scrolledBy = App.scrollTop;
+                let scrollDirection = true; // true = down, false = up
+
+                scrolledBy > this.scrollPos ? scrollDirection = true : scrollDirection = false;
+                
+                for (let i = 0; i < this.mainData.length; i++) {
+                    let val = 0;
+                    scrollDirection ? val = this.mainData[i].offsetTop - halfScreen : val = this.mainData[i].offsetTop + halfScreen;
+
+                    if (Math.abs(scrolledBy - val) < 100) {
+                        this.menuActive = this.mainData[i].Title;
+                        break;
+                    };
+                };
+
+                this.scrollPos = scrolledBy;
+            });
         },
         beforeUpdate: function() {
 
         },
         methods: {
+            goToMenu: function(title) {
+                let target = document.querySelector(`section.menu__position[data-title='${ title }']`).offsetTop;
+                let menu = document.querySelector(`section.menu`).offsetHeight;
+                let scrollLength = target - menu;
 
+                App.scrollTo({ top: scrollLength, behavior: 'smooth' });
+            },
         }
     });
 };
@@ -108,7 +153,7 @@ function convertExcelData(event) {
 }
 
 function generateMenuAndCards(data) {
-    let iterator = 1;
+    let iterator = 2;
 
     data.forEach(element => {
         if (element['Меню']) {
